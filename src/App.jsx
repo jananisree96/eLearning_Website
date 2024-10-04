fb02ba098b5d7f0b066f195dffc06ba19a6d3640
Janani Sree <153414043+jananisree96@users.noreply.github.com>
2024-10-04 20:24:49 +0530

    services added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from './components/Hero/Hero'
 import Banner from './components/Banner/Banner'
 import Banner2 from './components/Banner/Banner2'
+import Services from './components/Services/Services'
 
 
 function App() {
   return (
     <main className='overflow-x-hidden bg-white text-dark'>
       <Hero/>
+      <Services />
       <Banner />
       <Banner2/>
     </main>
