392ea930e013d5a7853d00fb0b1730cbf7615add
Janani Sree <153414043+jananisree96@users.noreply.github.com>
2024-10-04 20:21:43 +0530

    banner section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Hero from './components/Hero/Hero'
+import Banner from './components/Banner/Banner'
+import Banner2 from './components/Banner/Banner2'
 
 
 function App() {
   return (
     <main className='overflow-x-hidden bg-white text-dark'>
       <Hero/>
+      <Banner />
+      <Banner2/>
     </main>
   )
 }
